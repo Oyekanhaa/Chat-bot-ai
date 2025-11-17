859ce547e251e83fd3883c345718a74864a264d5
hccccrr <vrajiv830@gmail.com>
2025-11-17 22:44:01 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -12,15 +12,15 @@ API_HASH = "b35b715fe8dc0a58e8048988286fc5b6"
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 DB_NAME = "shizuDB"
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", "7009601543"))
+OWNER_ID = int(getenv("OWNER_ID", "7875273604"))
 BOT_ID = int(getenv("BOT_ID", "8113054986"))
 SUPPORT_GRP = "PBX_CHAT"
 UPDATE_CHNL = "HEROKUBIN_01"
-OWNER_USERNAME = "ll_BAD_MUNDA_ll"
+OWNER_USERNAME = "saniya_queen_ff"
 TIME_ZONE = "Asia/Kolkata"
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1002056907061"))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1003191450575"))
 # --------------------------------------------------------------
-SUDOERS = list(map(int, getenv("SUDOERS", "7009601543").split()))
+SUDOERS = list(map(int, getenv("SUDOERS", "7875273604").split()))
 # --------------------------------------------------------------
 
 ### DONT TOUCH or EDIT codes after this line
@@ -29,6 +29,6 @@ BANNED_USERS = filters.user()
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://gitlab.com/Badhacker98/Chatbot",
+    "https://github.com/korean009/Chat-bot-ai",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
